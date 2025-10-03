7dbc7c95ec31a25fcc0f6f2c6787c0f4f6827d7d
ramya.pothankar <ramya.pothankar@cognizant.com>
2025-10-03 07:28:16 +0000

    Add ionic cli install to the dev.nix file.

--- ionic/angular/dev.nix
+++ ionic/angular/dev.nix
@@ -18,7 +18,7 @@
       # Runs when a workspace is first created with this `dev.nix` file
       onCreate = {
         npm-install = "npm ci --no-audit --prefer-offline --no-progress --timing || npm i --no-audit --no-progress --timing";
-        # Open editors for the following files by default, if they exist:
+        ionic-cli-install = "npm install -g @ionic/cli";
         default.openFiles = [ "src/app/app.component.ts" ];
       };
       # To run something each time the workspace is (re)started, use the `onStart` hook
