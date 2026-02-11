179336c56feb942f369e3cf57e7684ba7037dda4
Karthi_CTS <karthi.m@cognizant.com>
2026-02-11 14:45:03 +0530

    Update idx-template.nix

--- qwik-city-vite/idx-template.nix
+++ qwik-city-vite/idx-template.nix
@@ -3,8 +3,25 @@
     pkgs.nodejs_20
   ];
   bootstrap = ''
+    set -e
     mkdir -p "$WS_NAME"
     npm create qwik@latest "empty" "$WS_NAME"
+    
+    # Pin @eslint/js to version 9 to avoid conflicts
+    # with eslint-plugin-qwik
+    cd "$WS_NAME"
+    node -e '
+      const fs = require("fs");
+      const path = "package.json";
+      const pkg = JSON.parse(fs.readFileSync(path, "utf-8"));
+      if (pkg.devDependencies && pkg.devDependencies["@eslint/js"]) {
+        console.log("Pinning @eslint/js to ^9.0.0");
+        pkg.devDependencies["@eslint/js"] = "^9.0.0";
+      }
+      fs.writeFileSync(path, JSON.stringify(pkg, null, 2));
+    '
+    cd ..
+    
     mkdir -p "$WS_NAME/.idx/"
     cp -rf ${./icon.png} "$WS_NAME/.idx/icon.png"
     cp -rf ${./dev.nix} "$WS_NAME/.idx/dev.nix"
