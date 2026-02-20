4286002cd9e24a02b00122b1c00177ee1958fa0a
RamyaPothankar <ramya.pothankar@cognizant.com>
2026-02-20 16:23:05 +0530

    Refactor ESLint configuration format
    Refactored ESLint configuration to use an array format and include recommended configurations from TypeScript and Astro plugins.

--- astro/.eslintrc.cjs
+++ astro/.eslintrc.cjs
@@ -1,30 +1,6 @@
-module.exports = {
-  root: true,
-  env: {
-    node: true,
-    es2022: true,
-    browser: true,
-  },
-  extends: ["eslint:recommended", "plugin:astro/recommended", "prettier"],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  overrides: [
-    {
-      files: ["*.astro"],
-      parser: "astro-eslint-parser",
-      parserOptions: {
-        parser: "@typescript-eslint/parser",
-        extraFileExtensions: [".astro"],
-      },
-      rules: {},
-    },
-    {
-      files: ["*.ts"],
-      parser: "@typescript-eslint/parser",
-      extends: ["plugin:@typescript-eslint/recommended"],
-      rules: {},
-    },
-  ],
-};
+module.exports = [
+  "eslint:recommended",
+  ...require("@typescript-eslint/eslint-plugin").configs.recommended,
+  ...require("eslint-plugin-astro").configs.recommended,
+  "prettier",
+];
