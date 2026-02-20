f3d38ceabfee6819ddd86bba4b1cf750da7d2af6
RamyaPothankar <ramya.pothankar@cognizant.com>
2026-02-20 17:20:06 +0530

    Delete astro/.eslintrc.cjs

--- astro/.eslintrc.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = [
-  "eslint:recommended",
-  ...require("@typescript-eslint/eslint-plugin").configs.recommended,
-  ...require("eslint-plugin-astro").configs.recommended,
-  "prettier",
-];
